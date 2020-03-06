b30414d3ec0728344ef03b2f3eac2d76d05dcdb7
Deepanshu Gupta <M1046873@mindtree.com>
2020-03-06 07:18:47 +0530

    login form added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [formValues, setFormValues] = useState(undefined);
+  const style = {
+    display: 'table-caption',
+    margin: '50%',
+    padding: '10%'
+  }
+  function submitForm(event) {
+    setFormValues({ email: event.target[0].value, password: event.target[1].value })
+    event.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={style}>
+      <h1>Login </h1>
+      <form onSubmit={(event) => submitForm(event)}>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" />
+        <input type="submit" value="Submit" />
+      </form>
+      {
+        formValues ?
+          <div>
+            <label>{formValues.email}</label>
+            <br />
+            <label>{formValues.password}</label>
+          </div >
+          : ''
+
+      }
     </div>
   );
 }
